1d5dab10e8991af40015fb7bd02f41a7b177f0e5
Jagriti <jagritim2002@gmail.com>
2023-03-01 02:42:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+mongoose.connect('mongodb+srv://jagriti:13022002@cluster0.nfknphm.mongodb.net/?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+var db = mongoose.connection;
+
+db.on('error',()=>console.log("Error in Connecting to Database"));
+db.once('open',()=>console.log("Connected to Database"))
+
+app.post("/sign_up",(req,res)=>{
+    var name = req.body.name;
+    var description = req.body.description;
+    var survey = req.body.survey;
+    var startdate = req.body.startdate;
+    var enddate = req.body.enddate;
+    var criteria = req.body.criteria;
+
+    var data = {
+        "name": name,
+        "description" : description,
+        "survey": survey,
+        "startdate" : startdate,
+        "enddate"   : enddate,
+        "criteria" : criteria
+    }
+
+    db.collection('users').insertOne(data,(err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Successfully");
+    });
+
+    return res.redirect('success.html')
+
+})
+
+
+app.get("/",(req,res)=>{
+    res.set({
+        "Allow-access-Allow-Origin": '*'
+    })
+    return res.redirect('index.html');
+}).listen(3000);
+
+
+console.log("Listening on PORT 3000");
